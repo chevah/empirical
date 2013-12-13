2dc32f2ac77ad34a929f50ecf9d5e630dec4b7fd
Adi Roiban <adi.roiban@chevah.com>
2013-12-13 14:19:39 +0200

    [#1856] Check current working folder for temporary files.

--- chevah/empirical/tests/normal/test_testcase.py
+++ chevah/empirical/tests/normal/test_testcase.py
@@ -3,6 +3,7 @@
 """
 Tests for ChevahTestCase.
 """
+import os
 import sys
 
 from twisted.internet import defer, reactor
@@ -293,8 +294,9 @@ class TestEmpiricalTestCase(EmpiricalTestCase):
         with self.assertRaises(AssertionError) as context:
             self.assertTempIsClean()
 
-        self.assertStartsWith(
-            u'Temporary folder is not clean. [', context.exception.message)
+        message = context.exception.message.decode('utf-8')
+        self.assertStartsWith(u'Temporary folder is not clean.', message)
+        self.assertContains(temp_segments[-1], message)
 
         self.assertFalse(mk.fs.exists(temp_segments))
 
@@ -380,6 +382,55 @@ class TestEmpiricalTestCase(EmpiricalTestCase):
         with self.listenPort(address, port):
 
             self.assertIsListening(address, port)
+
+    def check_assertWorkingFolderIsClean(self, content):
+        """
+        Common tests for assertWorkingFolderIsClean.
+        """
+
+        with self.assertRaises(AssertionError) as context:
+            self.assertWorkingFolderIsClean()
+
+        message = context.exception.message.decode('utf-8')
+        for member in content:
+            self.assertContains(member, message)
+
+        # Calling it again will not raise any error since the folder is clean.
+        self.assertWorkingFolderIsClean()
+
+    def test_assertWorkingFolderIsClean_with_folder(self):
+        """
+        An error is raised if current working folder contains a temporary
+        folder and folder is cleaned.
+        """
+        # Our compat filesystem API does not support creating files in
+        # current working directory so we use direct API call to OS.
+        name = mk.string()
+        os.mkdir(mk.fs.getEncodedPath(name))
+
+        self.check_assertWorkingFolderIsClean([name])
+
+    def test_assertWorkingFolderIsClean_with_file(self):
+        """
+        An error is raised if current working folder contains a temporary
+        file and file is cleaned.
+        """
+        name = mk.string()
+        open(mk.fs.getEncodedPath(name), 'a').close()
+
+        self.check_assertWorkingFolderIsClean([name])
+
+    def test_assertWorkingFolderIsClean_with_file_and_folder(self):
+        """
+        An error is raised if current working folder contains a temporary
+        folder and file, and folder and folder is cleaned.
+        """
+        file_name = mk.string()
+        folder_name = mk.string()
+        open(mk.fs.getEncodedPath(file_name), 'a').close()
+        os.mkdir(mk.fs.getEncodedPath(folder_name))
+
+        self.check_assertWorkingFolderIsClean([file_name, folder_name])
 
 
 class TestEmpiricalTestCaseSkipSetup(EmpiricalTestCase):
